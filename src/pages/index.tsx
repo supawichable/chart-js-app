8077e63b11ea2c2982fa5750051adcfa74f8fbc8
Yuya Sato <suzu6u10@gmail.com>
2023-01-10 13:43:40 +0900

    #3 dmsのアプリ名を削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,32 @@
-import Head from 'next/head'
-import { Inter } from '@next/font/google'
-import React from 'react'
+import Head from 'next/head';
+import { Inter } from '@next/font/google';
+import React from 'react';
 import Chart from 'chart.js/auto';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home(props: any) {
   React.useEffect(() => {
     let myChart: any = null;
     const cereals = props.cereals.map((cereal: any) => {
-      return { x: cereal.calories, y: cereal.carbo }
-    })
+      return { x: cereal.calories, y: cereal.carbo };
+    });
     const config: any = {
-      type: "scatter",
+      type: 'scatter',
       data: {
         datasets: [
           {
-            label: "80 Cereals",
+            label: '80 Cereals',
             backgroundColor: 'rgb(255, 99, 132)',
-            data: cereals 
-          }
+            data: cereals,
+          },
         ],
       },
       options: {
         plugins: {
           legend: {
-            position: 'bottom'
-          }
+            position: 'bottom',
+          },
         },
         scales: {
           x: {
@@ -39,8 +39,8 @@ export default function Home(props: any) {
                 weight: 'bold',
                 lineHeight: 1.2,
               },
-              padding: { top: 20, left: 0, right: 0, bottom: 0 }
-            }
+              padding: { top: 20, left: 0, right: 0, bottom: 0 },
+            },
           },
           y: {
             display: true,
@@ -52,42 +52,45 @@ export default function Home(props: any) {
                 weight: 'bold',
                 lineHeight: 1.2,
               },
-              padding: { top: 20, left: 0, right: 0, bottom: 0 }
-            }
+              padding: { top: 20, left: 0, right: 0, bottom: 0 },
+            },
           },
         },
       },
     };
-    myChart = new Chart(document.getElementById("myChart") as HTMLCanvasElement, config);
+    myChart = new Chart(
+      document.getElementById('myChart') as HTMLCanvasElement,
+      config
+    );
     return () => {
-      myChart.destroy()
-    }
-  }, [])
+      myChart.destroy();
+    };
+  }, []);
   return (
     <>
       <Head>
-        <title>dms-coding-test</title>
+        <title>coding-test</title>
         <meta name="description" content="DMSチームのコーディング課題です。" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <section style={{ padding: "10pt"}}>
+        <section style={{ padding: '10pt' }}>
           <h1>コーディング課題</h1>
           <p>シリアルのデータ</p>
-          <div style={{ width: "400pt" }}>
+          <div style={{ width: '400pt' }}>
             <canvas id="myChart" width="300" height="300"></canvas>
           </div>
         </section>
       </main>
     </>
-  )
+  );
 }
 
 export async function getServerSideProps(context: any) {
-  const response = await fetch('http://localhost:3000/api/cereals')
-  const cereals = await response.json()
+  const response = await fetch('http://localhost:3000/api/cereals');
+  const cereals = await response.json();
   return {
-    props: { cereals }
-  }
+    props: { cereals },
+  };
 }
