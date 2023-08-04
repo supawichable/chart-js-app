46761fdd64977464583495635e084ab59014c1d4
Supawich Jiarakul <s.jiarakul@gmail.com>
2023-08-04 11:51:14 +0000

    integreate and set up prettier, format files

--- README.md
+++ README.md
@@ -109,8 +109,8 @@ VSCodeを使われている場合は、拡張機能の「Dev Containers」を使
 ※もし初回でエラーが出たら一度 `docker compose up -d` を実行してみてください。
 
 1. Docker環境の起動 <br>
-  1-a. 通常起動する場合： `docker compose up -d` <br>
-  1-b. Dev Containersを使う場合： VSCodeの画面左下「><」みたいなボタンから「Reopen in Container」を選択 <br>
+   1-a. 通常起動する場合： `docker compose up -d` <br>
+   1-b. Dev Containersを使う場合： VSCodeの画面左下「><」みたいなボタンから「Reopen in Container」を選択 <br>
 1. `npm ci` を実行
 1. `npm run dev` を実行
 1. <http://localhost:3000/> を開く
@@ -133,7 +133,7 @@ Windowsの場合は以下の方法でMakeコマンドを準備できます。
 Windows11で動作確認してます。
 
 1. [Make for Windows](https://gnuwin32.sourceforge.net/packages/make.htm)からMakeのインストールファイルをダウンロードする。
-![image](https://user-images.githubusercontent.com/37053383/211447419-739f556a-fd79-4a6e-888f-a11ead2f79a0.png)
+   ![image](https://user-images.githubusercontent.com/37053383/211447419-739f556a-fd79-4a6e-888f-a11ead2f79a0.png)
 2. ダウンロードしたファイルをインストールする
 3. 環境変数にmake.exeのファイルパスを追加する。
    例：`C:\Program Files (x86)\GnuWin32\bin`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
-import Head from 'next/head';
-import { Inter } from '@next/font/google';
-import React from 'react';
-import Chart from 'chart.js/auto';
-import { numeralNutritions } from 'src/constants/numeralNutritions';
+import Head from "next/head";
+import { Inter } from "@next/font/google";
+import React from "react";
+import Chart from "chart.js/auto";
+import { numeralNutritions } from "src/constants/numeralNutritions";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home(props: any) {
   const nutritionArr = numeralNutritions;
-  const [xAxis, setXAxis] = React.useState('calories');
-  const [yAxis, setYAxis] = React.useState('carbo');
+  const [xAxis, setXAxis] = React.useState("calories");
+  const [yAxis, setYAxis] = React.useState("carbo");
 
   React.useEffect(() => {
     let myChart: any = null;
@@ -17,12 +17,12 @@ export default function Home(props: any) {
       return { x: cereal[xAxis], y: cereal[yAxis] };
     });
     const config: any = {
-      type: 'scatter',
+      type: "scatter",
       data: {
         datasets: [
           {
-            label: '80 Cereals',
-            backgroundColor: 'rgb(255, 99, 132)',
+            label: "80 Cereals",
+            backgroundColor: "rgb(255, 99, 132)",
             data: cereals,
           },
         ],
@@ -30,7 +30,7 @@ export default function Home(props: any) {
       options: {
         plugins: {
           legend: {
-            position: 'bottom',
+            position: "bottom",
           },
         },
         scales: {
@@ -38,10 +38,10 @@ export default function Home(props: any) {
             display: true,
             title: {
               display: true,
-              text: xAxis[0].toUpperCase().concat(xAxis.slice(1,)),
+              text: xAxis[0].toUpperCase().concat(xAxis.slice(1)),
               font: {
                 size: 20,
-                weight: 'bold',
+                weight: "bold",
                 lineHeight: 1.2,
               },
               padding: { top: 20, left: 0, right: 0, bottom: 0 },
@@ -51,10 +51,10 @@ export default function Home(props: any) {
             display: true,
             title: {
               display: true,
-              text: yAxis[0].toUpperCase().concat(yAxis.slice(1,)),
+              text: yAxis[0].toUpperCase().concat(yAxis.slice(1)),
               font: {
                 size: 20,
-                weight: 'bold',
+                weight: "bold",
                 lineHeight: 1.2,
               },
               padding: { top: 20, left: 0, right: 0, bottom: 0 },
@@ -64,8 +64,8 @@ export default function Home(props: any) {
       },
     };
     myChart = new Chart(
-      document.getElementById('myChart') as HTMLCanvasElement,
-      config
+      document.getElementById("myChart") as HTMLCanvasElement,
+      config,
     );
     return () => {
       myChart.destroy();
@@ -80,7 +80,7 @@ export default function Home(props: any) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <section style={{ padding: '10pt' }}>
+        <section style={{ padding: "10pt" }}>
           <h1>chart-js-app</h1>
           <p>シリアルのデータ</p>
           <div className="relative w-full lg:max-w-sm">
@@ -89,13 +89,15 @@ export default function Home(props: any) {
               <select
                 value={xAxis}
                 defaultValue={xAxis}
-                onChange={e => setXAxis(e.target.value)}
+                onChange={(e) => setXAxis(e.target.value)}
               >
-                {
-                  nutritionArr.map((nutrition: string, index) => {
-                    return <option value={nutrition} key={index}>{nutrition}</option>
-                  })
-                }
+                {nutritionArr.map((nutrition: string, index) => {
+                  return (
+                    <option value={nutrition} key={index}>
+                      {nutrition}
+                    </option>
+                  );
+                })}
               </select>
             </label>
           </div>
@@ -105,16 +107,19 @@ export default function Home(props: any) {
               <select
                 value={yAxis}
                 defaultValue={yAxis}
-                onChange={e => setYAxis(e.target.value)}>
-                {
-                  nutritionArr.map((nutrition: string, index) => {
-                    return <option value={nutrition} key={index}>{nutrition}</option>
-                  })
-                }
+                onChange={(e) => setYAxis(e.target.value)}
+              >
+                {nutritionArr.map((nutrition: string, index) => {
+                  return (
+                    <option value={nutrition} key={index}>
+                      {nutrition}
+                    </option>
+                  );
+                })}
               </select>
             </label>
           </div>
-          <div style={{ width: '400pt' }}>
+          <div style={{ width: "400pt" }}>
             <canvas id="myChart" width="300" height="300"></canvas>
           </div>
         </section>
@@ -124,7 +129,7 @@ export default function Home(props: any) {
 }
 
 export async function getServerSideProps(context: any) {
-  const response = await fetch('http://localhost:3000/api/cereals');
+  const response = await fetch("http://localhost:3000/api/cereals");
   const cereals = await response.json();
   return {
     props: { cereals },
